b71dc60ee6b8c4958a1c3d8b9753c8de3200a117
tian1152010110 <2282550468@qq.com>
2018-05-24 22:18:43 +0800

    20180524

---
+++ ChessFaceToFace2/app/src/main/java/com/example/junruitian/chessfacetoface/FirstActivity.java
@@ -0,0 +1,29 @@
+package com.example.junruitian.chessfacetoface;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this,"click success",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
