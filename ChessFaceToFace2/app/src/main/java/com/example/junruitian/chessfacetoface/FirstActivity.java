550887850d4cac00f8d19993071465f932934c4a
tian1152010110 <2282550468@qq.com>
2018-05-24 23:19:25 +0800

    201804242319

--- ChessFaceToFace2/app/src/main/java/com/example/junruitian/chessfacetoface/FirstActivity.java
+++ ChessFaceToFace2/app/src/main/java/com/example/junruitian/chessfacetoface/FirstActivity.java
@@ -3,6 +3,8 @@ package com.example.junruitian.chessfacetoface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,11 +21,44 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this,"click success",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
-                startActivity(intent);
+                Toast.makeText(FirstActivity.this,"begin success",Toast.LENGTH_SHORT).show();
+                Intent intent1 = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent1);
             }
         });
 
+        Button button3 = (Button) findViewById(R.id.button_3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(FirstActivity.this,"click success",Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                //startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.add_item:
+                Toast.makeText(this,"Login",Toast.LENGTH_SHORT).show();
+                //加入login后期activity
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"Logout",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
